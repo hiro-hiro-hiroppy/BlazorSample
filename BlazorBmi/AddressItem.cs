761b87387be2e92ad989d495c9ab6ae31d88a7b9
横田 裕之 <>
2021-12-24 22:16:27 +0900

    一旦コミット

---
+++ BlazorBmi/AddressItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BlazorBmi
+{
+    public class AddressItem
+    {
+        public string ZipCode { get; set; } = string.Empty;
+        public string Prefecture { get; set; } = string.Empty;
+        public string Telephone { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int Age { get; set; }
+    }
+}
+
